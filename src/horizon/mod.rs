e6969b87a2899fff7ff099dc6725821945b9afe2
Francesco Ceccon <francesco@ceccon.me>
2020-07-24 15:49:40 +0100

    feat: add operation endpoint
    Signed-off-by: Francesco Ceccon <francesco@ceccon.me>

--- src/horizon/mod.rs
+++ src/horizon/mod.rs
@@ -1,11 +1,32 @@
 use crate::config::AppConfig;
+use crate::error::Error;
+use crate::render::ResponseRender;
 use anyhow::Result;
 use convey::Output;
+use serde::ser::Serialize;
+use stellar_horizon::client::HorizonClient;
+use stellar_horizon::request::{Order, PageRequest, Request, StreamRequest};
 use structopt::StructOpt;
+use tokio::stream::StreamExt;
 
 mod account;
 mod ledger;
+mod operation;
 mod server;
+
+#[derive(Debug, StructOpt)]
+pub struct Paging {
+    #[structopt(long, help = "Start from this ledger")]
+    pub cursor: Option<String>,
+    #[structopt(long, help = "Limit the number of ledgers returned")]
+    pub limit: Option<u64>,
+    #[structopt(long, help = "Return ledgers in ascending order", group = "order")]
+    pub ascending: bool,
+    #[structopt(long, help = "Return ledgers in descending order", group = "order")]
+    pub descending: bool,
+    #[structopt(long, help = "Stream response")]
+    pub stream: bool,
+}
 
 #[derive(Debug, StructOpt)]
 #[structopt(about = "Call Horizon endpoints")]
@@ -27,6 +48,7 @@ pub enum HorizonInnerCommand {
 pub enum HorizonNonServerCommand {
     Account(account::AccountCommand),
     Ledger(ledger::LedgerCommand),
+    Operation(operation::OperationCommand),
 }
 
 pub async fn run_command(
@@ -46,7 +68,66 @@ pub async fn run_command(
                 HorizonNonServerCommand::Ledger(cmd) => {
                     ledger::run_command(&mut out, &config, &client, cmd).await
                 }
+                HorizonNonServerCommand::Operation(cmd) => {
+                    operation::run_command(&mut out, &config, &client, cmd).await
+                }
             }
         }
+    }
+}
+
+pub fn add_paging_options<R: PageRequest>(mut request: R, options: &Paging) -> R {
+    if let Some(cursor) = options.cursor.as_ref() {
+        request = request.with_cursor(cursor);
+    }
+
+    if let Some(limit) = options.limit {
+        request = request.with_limit(limit);
+    }
+
+    if options.ascending {
+        request = request.with_order(&Order::Ascending);
+    }
+
+    if options.descending {
+        request = request.with_order(&Order::Descending);
+    }
+
+    request
+}
+
+pub async fn execute_and_print_request<H, R>(out: &mut Output, client: &H, request: R) -> Result<()>
+where
+    H: HorizonClient,
+    R: Request,
+    R::Response: Serialize,
+{
+    let (_, response) = client.request(request).await?;
+    out.print(ResponseRender(response)).map_err(Error::Convey)?;
+    Ok(())
+}
+
+pub async fn execute_and_print_page_request<H, R>(
+    mut out: &mut Output,
+    client: &H,
+    mut request: R,
+    paging: &Paging,
+) -> Result<()>
+where
+    H: HorizonClient,
+    R: StreamRequest + PageRequest + 'static,
+    R::Response: Serialize,
+    R::Resource: Serialize,
+{
+    request = add_paging_options(request, &paging);
+
+    if paging.stream {
+        let mut stream = client.stream(request)?;
+        while let Some(event) = stream.try_next().await? {
+            out.print(ResponseRender(event)).map_err(Error::Convey)?;
+        }
+        Ok(())
+    } else {
+        execute_and_print_request(&mut out, client, request).await
     }
 }
--- src/horizon/operation.rs
+++ src/horizon/operation.rs
@@ -7,22 +7,24 @@ use stellar_horizon::client::HorizonClient;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
-#[structopt(about = "Horizon ledger endpoints")]
-pub enum LedgerCommand {
-    All(AllLedgersCommand),
-    Single(SingleLedgerCommand),
+#[structopt(about = "Horizon operation endpoints")]
+pub enum OperationCommand {
+    All(AllOperationsCommand),
+    Single(SingleOperationCommand),
 }
 
 #[derive(Debug, StructOpt)]
-#[structopt(about = "Retrieves information about a single ledger")]
-pub struct SingleLedgerCommand {
-    #[structopt(name = "LEDGER_ID", help = "The ledger id")]
-    pub ledger_id: i32,
+#[structopt(about = "Retrieves information about a single operation")]
+pub struct SingleOperationCommand {
+    #[structopt(name = "OPERATION_ID", help = "The operation id")]
+    pub operation_id: i32,
 }
 
 #[derive(Debug, StructOpt)]
-#[structopt(about = "Retrieves information about a list of ledgers")]
-pub struct AllLedgersCommand {
+#[structopt(about = "Retrieves information about a list of operations")]
+pub struct AllOperationsCommand {
+    #[structopt(long, help = "Include failed operations")]
+    pub include_failed: bool,
     #[structopt(flatten)]
     pub paging: Paging,
 }
@@ -31,14 +33,14 @@ pub async fn run_command<H>(
     mut out: &mut Output,
     config: &AppConfig,
     client: &H,
-    command: LedgerCommand,
+    command: OperationCommand,
 ) -> Result<()>
 where
     H: HorizonClient,
 {
     match command {
-        LedgerCommand::All(cmd) => run_all(&mut out, &config, client, cmd).await,
-        LedgerCommand::Single(cmd) => run_single(&mut out, &config, client, cmd).await,
+        OperationCommand::All(cmd) => run_all(&mut out, &config, client, cmd).await,
+        OperationCommand::Single(cmd) => run_single(&mut out, &config, client, cmd).await,
     }
 }
 
@@ -46,12 +48,12 @@ pub async fn run_all<H>(
     mut out: &mut Output,
     _config: &AppConfig,
     client: &H,
-    command: AllLedgersCommand,
+    command: AllOperationsCommand,
 ) -> Result<()>
 where
     H: HorizonClient,
 {
-    let request = api::ledgers::all();
+    let request = api::operations::all().with_include_failed(command.include_failed);
     execute_and_print_page_request(&mut out, client, request, &command.paging).await
 }
 
@@ -59,11 +61,11 @@ pub async fn run_single<H>(
     mut out: &mut Output,
     _config: &AppConfig,
     client: &H,
-    command: SingleLedgerCommand,
+    command: SingleOperationCommand,
 ) -> Result<()>
 where
     H: HorizonClient,
 {
-    let request = api::ledgers::single(command.ledger_id);
+    let request = api::ledgers::single(command.operation_id);
     execute_and_print_request(&mut out, client, request).await
 }
